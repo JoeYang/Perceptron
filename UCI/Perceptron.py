1c1dd6819a642c995d954bde1013ee66e8e3aa93
joeyang <xyan2284@gmail.com>
2012-05-02 23:19:02 +1000

    UCI functioning

--- UCI/Perceptron.py
+++ UCI/Perceptron.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import random
 
 class PerceptronModel:
   
@@ -22,21 +23,20 @@ class PerceptronModel:
   class Klass:
   	def __init__(self, name):
   	  self.name = name
-  	  self.new_weights = []
   	  self.weights = []
   	  self.average_weights = []
   	  self.num_interations = 0
   	  self.num_of_features = 1555
   	  for i in range(0, self.num_of_features):
   	  	self.weights.append(0) 
-  	  	self.new_weights.append(0)
+  	  	#self.new_weights.append(0)
   	  	self.average_weights.append(0)
   	
   	def update(self):
   	  for i in range(0, self.num_of_features):
-  	  	self.average_weights[i] += self.new_weights[i]
-  	  	self.weights[i] = self.new_weights[i]
-  	  	self.new_weights[i] = 0
+  	  	self.average_weights[i] += self.weights[i]
+  	  	#self.weights[i] = self.new_weights[i]
+  	  	
   	  self.num_interations += 1	
 
   	def get_average(self):
@@ -48,7 +48,7 @@ class PerceptronModel:
   	def reset(self):  
   	  for i in range(0, self.num_of_features):
   	  	self.weights[i] = 0 
-  	  	self.new_weights[i] = 0 
+  	  	#self.new_weights[i] = 0 
   	  	self.average_weights[i] = 0 
   
   def __init__(self):
@@ -72,7 +72,8 @@ class PerceptronModel:
   	  split = self.TrainSplit()
   	  length = len(self.instances)
   	  for i in range(0, length):
-  	  	if i % self.num_of_folds == fold:
+  	  	
+  	  	if i % self.num_of_folds != fold:
   	  	  split.train.append(self.instances[i])
   	  	else:
   	  	  split.test.append(self.instances[i])
@@ -87,24 +88,24 @@ class PerceptronModel:
   	TP = 0.0
   	FP = 0.0
   	FN = 0.0
+  	TN = 0.0
   	for instance in split.test:
   	  klass = self.classify(instance)
   	  #print klass.name, instance.real_tag
   	  if klass.name == 'ad.':
   	  	if instance.real_tag == 'nonad.':
   	  	  FP += 1.0
-  	  	
+  	  	else:
+  	  	  TP += 1.0  
   	  if klass.name == "nonad.":
   	  	if instance.real_tag == "nonad.":
-  	  	  TP += 1.0
+  	  	  TN += 1.0
   	  	else:
   	  	  FN += 1.0
   	P = TP/(TP + FP)
   	R = TP/(TP + FN)
-  	print FP, FN
-  	print "Precision", P,
-	print "Recall", R,
-	print "F-score", 2*P*R/(P + R)
+  	print TP, FP, FN, TN
+	print "F-score", 1-(FP + FN)/(TP + TN + FP + FN)
   
   def classify(self, instance):
   	best_score = -100000
@@ -116,14 +117,12 @@ class PerceptronModel:
   	  if score > best_score:
   	  	best_score = score
   	  	maximum_likely_klass = klass
-  	  #print score, klass.name,
-  	#print maximum_likely_klass.name, instance.real_tag  
   	return maximum_likely_klass
   
   def train(self, split):
   	iteration = 0
   	total = len(split.train)
-  	while iteration < 50:
+  	while iteration < 20:
   	  error = 0
   	  for instance in split.train:
   	  	klass = self.get_correct_klass(instance.real_tag)
@@ -131,10 +130,11 @@ class PerceptronModel:
   	  	if guess.name != klass.name:
   	  	  error += 1  	  	  
    	  	  for i in range(0, self.num_of_features):
-  	  	  	klass.new_weights[i] = klass.weights[i] + 0.3*instance.features[i]
-  	  	  	guess.new_weights[i] = guess.weights[i] - 0.1*instance.features[i]
+  	  	  	klass.weights[i] += instance.features[i]
+  	  	  	#guess.weights[i] -= instance.features[i]
   	  	  klass.update()
   	  	  guess.update()  	
+  	  #print "Iteration:", iteration, "error:", error
   	  iteration += 1
   	 
 
@@ -157,10 +157,10 @@ def main():
   index = 1
   for split in splits:
   	print "Cross Validation", index, ":",
-  	pm.train(split)
-  	#for klass in pm.klasses:
-  	#  klass.get_average()
-  	pm.test(split)
+  	pm.train(splits[2])
+  	for klass in pm.klasses:
+  	  klass.get_average()
+ 	pm.test(split)
   	pm.reset()
   	index += 1
 
